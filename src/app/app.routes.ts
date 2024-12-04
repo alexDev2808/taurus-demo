eb31a53562c0b8080d1244eb9659cf34af11d55c
tauesp127-alexis <gestionti@taurus.com.mx>
2024-12-04 12:10:14 -0600

    Rutas hijas y lazyload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then( m => m.HomeModule)
+    },
+    {
+        path: 'upload',
+        loadChildren: () => import('./upload/upload.module').then( m => m.UploadModule )
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
